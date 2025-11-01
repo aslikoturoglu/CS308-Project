68aa990ca291ce8e355378d3dac7e47df3f372fe
Işık Giray Önal <isik.onal@sabanciuniv.edu>
2025-11-01 17:44:01 +0300

    General Project Template
    Project Template Upload

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Backend çalışıyor!"));
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Server ${process.env.PORT || 5000} portunda`)
+);
