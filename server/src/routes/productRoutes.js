f00a786170f1d8c64444601e5153291dfa928c03
mehmetsefaciftci <mehmet.ciftci@sabanciuniv.edu>
2025-12-08 13:38:51 +0300

    Merge conflicts resolved

--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -1,4 +1,3 @@
-// server/src/routes/productRoutes.js
 import { Router } from "express";
 import {
   getAllProducts,
@@ -11,22 +10,11 @@ import {
 
 const router = Router();
 
-// Tüm ürünleri getir
 router.get("/", getAllProducts);
-
-// Tek ürün getir
 router.get("/:id", getProductById);
-
-// Yeni ürün ekle
 router.post("/", addProduct);
-
-// Ürün güncelle
 router.put("/:id", updateProduct);
-
-// Ürün sil
 router.delete("/:id", deleteProduct);
-
-// Stok artır / azalt
 router.put("/:id/stock", updateProductStock);
 
 export default router;
