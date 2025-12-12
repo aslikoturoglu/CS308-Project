47b45aefcabe1b8f5b1f3a68572045b930cf2f1a
Işık Giray Önal <isik.onal@sabanciuniv.edu>
2025-12-12 16:19:55 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, useLocation } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -8,22 +8,29 @@ import AppRouter from "./router/AppRouter";
 
 import "./styles/globals.css";
 
+function AppChrome() {
+  const location = useLocation();
+  const hideShell = location.pathname.startsWith("/admin");
 
+  return (
+    <div className="app-shell">
+      {!hideShell && <Header />}
+      {!hideShell && <Navbar />}
+
+      <div className="app-content">
+        <AppRouter />
+      </div>
+
+      {!hideShell && <ChatButton />}
+      {!hideShell && <Footer />}
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <div className="app-shell">
-        <Header />
-        <Navbar />
-
-        <div className="app-content">
-          <AppRouter />
-        </div>
-
-        <ChatButton />
-        <Footer />
-      </div>
+      <AppChrome />
     </Router>
   );
 }
