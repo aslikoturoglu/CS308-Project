237f884576ef4eda4192120317f6ff813c50e7b5
Aslı <asli.koturoglu@sabanciuniv.edu>
2025-12-28 02:52:12 +0300

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { fetchProductsWithMeta } from "../services/productService";
 import gsap from "gsap";
